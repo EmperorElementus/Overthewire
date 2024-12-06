97448a952388572bb86ba6220eef9c55bef2673f
KokuShibo <161367633+EmperorElementus@users.noreply.github.com>
2024-12-06 12:51:11 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -166,4 +166,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     generateRestaurantCards();
+	
+	const swiper = new Swiper('.swiper', {
+		loop: true,
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
 });
