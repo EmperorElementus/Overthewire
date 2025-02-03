7ef8a3c95cd3983c2622cbb501d94f98f8d6362c
KokuShibo <161367633+EmperorElementus@users.noreply.github.com>
2025-02-03 13:19:02 +0200

    Add files via upload

--- js/app/router.js
+++ js/app/router.js
@@ -4,7 +4,7 @@ import {campaigns} from './pages/campaigns.js';
 export const router = VueRouter.createRouter({
     history: VueRouter.createWebHashHistory(),
     routes: [
-    { path: '/', name: 'Sign in', component: login },
-    { path: '/campaigns', name: 'Campaigns', component: campaigns },
+        { path: '/', name: 'Sign in', component: login },
+        { path: '/campaigns', name: 'Campaigns', component: campaigns },
     ]
- });
+});
