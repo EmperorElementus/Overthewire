ab146efdf07bfb8a60bc16fecb2c1a746a8abddd
KokuShibo <161367633+EmperorElementus@users.noreply.github.com>
2025-02-05 14:49:32 +0200

    Add files via upload

--- js/app/app.js
+++ js/app/app.js
@@ -1,26 +1,30 @@
-import { router } from './router.js';
-import { header } from './widgets/header.js';
-import { msg } from './widgets/msg.js';
-import { popup } from './widgets/popup.js';
+import {router} from './router.js';
+import {msg} from './widgets/msg.js';
+import {header} from './widgets/header.js';
+// import {search} from './widgets/search.js';
+import {popup} from './widgets/popup.js';
+
+import {toogle} from './widgets/toogle.js';
+// import {img} from './widgets/img.js';
 
 document.addEventListener('DOMContentLoaded', function(){
     const main = {
         data() {
             return {
                 url:"https://affiliate.yanbasok.com",
-                user: { name: "", phone: "", email: "", date: "", auth: "" },
-                formData: {},
-                title: "",
-                date: "",
-                time: "",
+                user: {name:"", phone:"", email:"", date:"", auth:""},
+                formData:{},
+                title:"",
+                date:"",
+                time:"",
+            }
+        },      
+        watch:{
+            $route:function(){
+                this.init();
             }
         },
-        watch: {
-            $route: function() {
-              this.init();
-            }
-        },
-        mounted: function() {
+        mounted:function(){
             this.init();
         },
         methods:{
@@ -29,78 +33,63 @@ document.addEventListener('DOMContentLoaded', function(){
                 if (window.localStorage.getItem('user')) self.user = JSON.parse(window.localStorage.getItem('user'));
 
                 router.isReady().then(() => {
-                    if (window.localStorage.getItem("user")) {
-                        self.user = JSON.parse(window.localStorage.getItem("user"));
-                        if (self.$route['path'] == '/' && self.user.type == 'admin') {
+                    if(window.localStorage.getItem("user")){
+                        self.user= JSON.parse(window.localStorage.getItem("user"));
+                        if (self.$route['path']=='/' && self.user.type=='admin'){
                             self.page('/campaigns');
-                        } else if (['/campaigns', '/campaign', '/users', '/user'].includes(self.$route['path'] ) && self.user.type != 'admin') {
+                        }else if(['/campaigns','/campaign', '/users', '/user'].includes (self.$route['path']) && self.user.type!='admin'){
                             self.page('/statistics');
-                        } else if (['/statistics', '/payments', '/sites'].includes(self.$route['path'] ) && self.user.user.type == 'admin') {
+                        }else if(['/statistics','/payments','/sites'].includes (self.$route ['path']) && self.user.type=='admin'){
                             self.page('/campaigns');
-                        } else if (['/campaigns', '/campaign', '/users', '/user', '/statistics', '/payments', '/sites'].includes(self.$route['path'] )) {
+                        }else if(['/campaigns','/campaign', '/users','/user', '/statistics','/payments','/sites'].includes(self.$route ['path']) && self.user.type=='admin'){
                             self.page();
-                        } else if (!['/campaigns', '/campaign', '/users', '/user', '/statistics', '/payments', '/sites'].includes(self.$route['path'] )) {
+                        }else if(!['/campaigns','/campaign','/users','/user','/statistics','/payments', '/sites'].includes (self.$route ['path']) && self.user.type=='admin'){
                             self.page();
                         }
-                    } else {
-                    self.page('/');
+                    }else{
+                        self.page('/');
                     }
                 });
             },
-            logout(){
-                this.user = {name:"", phone:"", email:"", date:"", auth:""};
-                this.page('/');
-                window.localStorage.setItem('user','');
-            },
-            scrollTop(){
-                setTimeout(function(){
-                    window.scroll({
-                        top: 0,
-                        behavior: 'smooth'
-                    });
-                },50);
-            },
-            scrollBottom(){
-                setTimeout(function(){
-                    window.scroll({
-                        top: 1000,
-                        behavior: 'smooth'
-                    });
-                },50);
-            },
-            page:function(path=""){
-                this.$router.replace(path);
-                this.title=this.$route['name'];
-                document.title=this.$route['name'];
-            },
-            toFormData:function(obj){
-                var fd = new FormData();
+        logout(){
+            this.user = {name:"", phone:"", email:"", date:"", auth:""};
+            this.page('/');
+            window.localStorage.setItem('user','');
+        },
+        page: function (path=""){
+            this.$router.replace(path);
+            this.title=this.$route['name'];
+            document.title=this.$route ['name'];
+        },
+        toFormData(obj) {
+            var fd = new FormData();
 
-                for(var x in obj) {
-                    if(typeof obj[x] === 'object' && x !== 'img' && x !== 'copy') {
-                        for(var y in obj[x]) {
-                            if(typeof obj[x] === 'object') {
-                                for(var z in obj[x][y]) {
-                                    fd.append(x + '[' + y + '][' + z + ']', obj[x][y][z]);
-                                }
-                            } else {
-                                fd.append(x + '[' + y + ']', obj[x][y]);
+            for (var x in obj) {
+                if (typeof obj[x] === 'object' && x !== 'img' && x !== 'copy') {
+                    for (var y in obj[x]) {
+                        if (typeof obj[x]==='object') {
+                            for (var z in obj[x][y]) {
+                                fd.append(x+'['+y+']['+z+']', obj[x][y][z]);
                             }
+                        } else {
+                            fd.append(x+'['+y+']', obj[x][y]);
                         }
-                    } else if(x != 'copy') {
-                        fd.append(x, obj[x]);
                     }
+                }else if (x != 'copy') {
+                    fd.append(x, obj[x]);
                 }
-            
-                return fd;
             }
-        }
-    };
 
+            return fd;
+          }
+        }
+      };
+    
     var app = Vue.createApp(main)
     .component('Header',header)
     .component('popup',popup)
     .component('msg',msg)
+    .component('toogle',toogle)
     .use(router)
     .mount('#content')
 });
